2d9844af72f83149b7724e885e191ea4a93ba909
Elizabeth <elizabethtreahy@gmail.com>
2022-03-15 18:55:10 -0400

    commit tree 73e644896a9951a4dcc15f41986d58d162c7f6a9

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution in this file!
+var customerName = 'bob'
+var bestCustomer = undefined
+const leastFavoriteCustomer = 'apple'
+function fixTheScope (){
+  customerName = 'bob'
+  return customerName
+}
+function fixTheCustomer() {
+  bestCustomer = undefined
+  return bestCustomer
+}
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'orange'
+  return leastFavoriteCustomer
+}
+function setBestCustomer () {
+  bestCustomer = 'not bob'
+  return bestCustomer
+}
+console.log(customerName)
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase()
+  return customerName
+}
+upperCaseCustomerName()
+console.log(customerName)
+fixTheScope()
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob'
+  return bestCustomer
+}
+overwriteBestCustomer()
+fixTheCustomer()
+changeLeastFavoriteCustomer()
